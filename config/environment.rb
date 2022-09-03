7da857adebc33dace85fa21034104be2d3af2b67
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-09-04 01:06:26 +0300

    commit tree 5ca95236f9e9442ae22826fa7fa1bb22fb6bcb5a

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,17 @@
 ENV["RACK_ENV"] ||= "development"
+require 'rake'
+require 'active_record'
+require 'yaml/store'
+require 'ostruct'
+require 'date'
 
 require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
+Bundler.require(:default, ENV["RACK_ENV"]) 
 
 require_relative "../app/models/artist"
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+) 
